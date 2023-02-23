15152904ef1510c9e913202875c598a76a4159e9
Oscar francisco Lopez escobar <olopeze@outlook.com>
2023-02-22 21:47:33 -0600

    fixing burger button

--- naval-arquitectura/src/ts/components/mainlayout/MainLayout.styled.js
+++ naval-arquitectura/src/ts/components/mainlayout/MainLayout.styled.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 const MainLayoutWrapper = styled.div`
   position: fixed;
   z-index: 2;
-  height: 100%;
+  height: 97%;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
   padding-left: 1rem;
+  padding-bottom: 1rem;
 `;
 
 const BurgerButton = styled.button`
-  width: 40px;
-  height: 30px;
   background: transparent;
   cursor: pointer;
 
   display: grid;
+  gap: 4px;
 
   > span {
     height: 4px;
